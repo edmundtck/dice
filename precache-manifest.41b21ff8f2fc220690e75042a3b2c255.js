03eba15e3522ac5e2e0cec4bb1de3aab70907adc
Edmund Teo <edmundteo.ck@gmail.com>
2020-10-13 19:27:49 +0800

    Updates

---
+++ precache-manifest.41b21ff8f2fc220690e75042a3b2c255.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5843b120cf748a9f9712b5d13d041298",
+    "url": "/dice/index.html"
+  },
+  {
+    "revision": "fce761bf4364d82df567",
+    "url": "/dice/static/css/main.627404e3.chunk.css"
+  },
+  {
+    "revision": "12f8f360e57227cfe91a",
+    "url": "/dice/static/js/2.3a1a4e2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dice/static/js/2.3a1a4e2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fce761bf4364d82df567",
+    "url": "/dice/static/js/main.30d350a9.chunk.js"
+  },
+  {
+    "revision": "6f349f4a9e93576960a4",
+    "url": "/dice/static/js/runtime-main.6581301c.js"
+  }
+]);
